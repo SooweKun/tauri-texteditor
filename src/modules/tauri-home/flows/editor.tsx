9eaf64d463ff48dc244b857da6aa4f1141f4be68
SooweKun <apextoplega@mail.ru>
2026-02-13 20:55:26 +0300

    feat: add theme for editor and update extensions for editor

--- src/modules/tauri-home/flows/editor.tsx
+++ src/modules/tauri-home/flows/editor.tsx
@@ -1,8 +1,10 @@
 'use client';
+import { history, historyKeymap, indentWithTab } from '@codemirror/commands';
 import { markdown } from '@codemirror/lang-markdown';
-import { EditorView } from '@codemirror/view';
+import { bracketMatching, defaultHighlightStyle, indentOnInput, syntaxHighlighting } from '@codemirror/language';
+import { EditorState } from '@codemirror/state';
+import { drawSelection, EditorView, keymap } from '@codemirror/view';
 import { skipToken, useQuery } from '@tanstack/react-query';
-import { basicSetup } from 'codemirror';
 import { useEffect, useRef } from 'react';
 
 export const Editor = () => {
@@ -17,15 +19,58 @@ export const Editor = () => {
 
   console.log(content, 'content');
 
+  const myTheme = EditorView.theme({
+    '&': {
+      height: '100%', // Или '100%'
+      width: '900px',
+      fontSize: '16px',
+    },
+    '.cm-content': {
+      fontFamily: 'Menlo, Monaco, Lucida Console, monospace',
+      padding: '10px 0',
+    },
+    '.cm-cursor, .cm-dropCursor': {
+      borderLeftColor: 'white',
+    },
+    // Убираем дефолтную синюю обводку при фокусе
+    '&.cm-focused': { outline: 'none' },
+    // Стили для самого текста внутри
+    '.cm-line': { padding: '0 10px' },
+  });
+
+  const extensions = [
+    history(), // Undo/Redo
+    drawSelection(), // Красивое выделение
+    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+    markdown(), // Поддержка Markdown
+    bracketMatching(), // Подсветка парных скобок
+    indentOnInput(), // Авто-отступ при вводе
+    EditorView.lineWrapping, // Автоматический перенос длинных строк
+    myTheme, // Твои стили
+    keymap.of([
+      ...historyKeymap,
+      indentWithTab, // Tab работает как отступ, а не прыжок по кнопкам
+    ]),
+  ];
+
+  const state = EditorState.create({
+    doc: content as string,
+    extensions,
+  });
+
   useEffect(() => {
     view.current = new EditorView({
-      doc: content as string,
-      extensions: [basicSetup, markdown(), EditorView.theme({ '&': { height: '100%', fontSize: '18px' } })],
+      state,
       parent: editorRef.current!,
     });
 
     return () => view.current?.destroy();
   }, [content]);
 
-  return <div ref={editorRef} className='rounded-[5px]' />;
+  return (
+    <div className='size-full flex justify-start items-center flex-col'>
+      <h1 className='text-xl'>{content?.name}</h1>
+      <div ref={editorRef} />
+    </div>
+  );
 };
