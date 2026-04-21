7a2203d1c3271a102405635f7223188682071e75
SooweKun <apextoplega@mail.ru>
2026-04-21 18:57:35 +0300

    feat: add animation on moone

--- src/modules/web-home/feature/animate.tsx
+++ src/modules/web-home/feature/animate.tsx
@@ -2,10 +2,10 @@ export const Animate = () => {
   return (
     <div
       className='relative size-[600px] rounded-full bg-black flex justify-center items-center
-                      shadow-[inset_0_-60px_100px_-30px_rgba(124,58,237,0.7),inset_0_-10px_30px_-5px_rgba(167,139,250,0.9)] 
-                      overflow-hidden'>
-      <p className='absolute text-[20px] text-shadow-sidebar-accent-foreground'>Tauri</p>
-      <div className='absolute inset-0 bg-[radial-gradient(circle_at_50%_120%,rgba(88,28,135,0.4)_0%,transparent_70%)]' />
+                   animate-moon-glow overflow-hidden'>
+      <p className='relative text-[20px] text-white/80 tracking-[0.3em] font-light uppercase'>Tauri</p>
+      <div className='absolute inset-0 bg-[radial-gradient(circle_at_50%_125%,rgba(88,28,135,0.5)_0%,transparent_70%)]' />
+      <div className='absolute inset-0 opacity-[0.12] mix-blend-overlay pointer-events-none' />
     </div>
   );
 };
