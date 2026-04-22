dc7eacd6ec97db7312c57a733263170f5f434658
SooweKun <apextoplega@mail.ru>
2026-04-22 10:26:43 +0300

    upd:update ui in web header

--- src/components/layouts/web-header.tsx
+++ src/components/layouts/web-header.tsx
@@ -6,13 +6,21 @@ export const WebHeader = () => {
   return (
     <div className='w-full h-[65px] px-[85px] flex items-center justify-between'>
       <div className='w-max px-2 flex gap-0 items-end'>
-        <p className='text-[16px] text-white'>Tauri-texteditor</p>
+        <p className='text-[16px] text-white/80 tracking-[0.3em] font-light uppercase'>Tauri</p>
       </div>
-      <div className='w-max p-2 flex gap-5'>
-        <Link href=''>profile</Link>
-        <Link href=''>cloud st</Link>
-        <Link href=''>settings</Link>
-        <Link href=''>dock</Link>
+      <div className='w-[600px] p-2 flex gap-6 text-white/80 tracking-[0.3em] font-light uppercase justify-center ml-[80px]'>
+        <Link href='' className='hover:text-white'>
+          profile
+        </Link>
+        <Link href='' className='hover:text-white'>
+          cloud st
+        </Link>
+        <Link href='' className='hover:text-white'>
+          settings
+        </Link>
+        <Link href='' className='hover:text-white'>
+          dock
+        </Link>
       </div>
       <div className='flex gap-[8px] text-white items-center'>
         {isRegistered ? (
@@ -20,9 +28,13 @@ export const WebHeader = () => {
           <div />
         ) : (
           <>
-            <Link href='/authorization'>sing in</Link>
-            <span className='w-[0.5px] h-[31px] bg-white rounded-2xl' />
-            <Link href='/registration'>sing up</Link>
+            <Link href='/authorization' className='text-white/80 tracking-[0.3em] font-light uppercase'>
+              sing in
+            </Link>
+            <span className='w-[0.5px] h-[31px] bg-white/80 rounded-2xl' />
+            <Link href='/registration' className='text-white/80 tracking-[0.3em] font-light uppercase'>
+              sing up
+            </Link>
           </>
         )}
       </div>
