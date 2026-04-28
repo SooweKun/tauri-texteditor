fa4bc84ac61a47d4b02499fb75f9d7a3c78ab476
SooweKun <apextoplega@mail.ru>
2026-04-28 21:49:34 +0300

    feat: +

--- src/modules/web-home/module.tsx
+++ src/modules/web-home/module.tsx
@@ -4,8 +4,11 @@ import { Animate } from './feature/animate';
 export const WebHomeModule = () => {
   return (
     <WebHeaderLayout>
-      <div className='text-white flex-1 flex justify-center items-center'>
-        <Animate />
+      <div className='text-white  flex flex-col '>
+        <div className='flex w-full h-screen justify-center items-center'>
+          <Animate />
+        </div>
+        <div className='w-full bg-black h-[900px]'></div>
       </div>
     </WebHeaderLayout>
   );
